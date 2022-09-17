77c8a3b8219818736774276cf18c938efe800c84
Jemin Hwangbo <jemin.hwangbo>
2022-09-18 07:46:46 +0900

    added the number of disks as argument

--- src/value_iteration_tutorial.cpp
+++ src/value_iteration_tutorial.cpp
@@ -2,6 +2,13 @@
 #include <Eigen/Core>
 #include <Eigen/Dense>
 
-int main() {
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    std::cout << "Error: You need one argument." << std::endl;
+    return 0;
+  }
+
+  const int n = std::stoi(argv[1]); // number of disks
+
   return 0;
 }
